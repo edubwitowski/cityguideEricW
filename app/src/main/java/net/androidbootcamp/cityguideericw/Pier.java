37c66458edc9bae468e33151cbfad859a34f3d59
Eric Witowski <ewitowski@students.mchenry.edu>
2018-02-25 11:02:36 -0600

    Initial commit

---
+++ app/src/main/java/net/androidbootcamp/cityguideericw/Pier.java
@@ -0,0 +1,16 @@
+package net.androidbootcamp.cityguideericw;
+
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Pier extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pier);
+    }
+
+}
+
---
+++ app/src/main/java/net/androidbootcamp/cityguideericw/Sears.java
@@ -0,0 +1,18 @@
+package net.androidbootcamp.cityguideericw;
+
+import android.os.Bundle;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+
+
+public class Sears extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sears);
+    }
+
+}
